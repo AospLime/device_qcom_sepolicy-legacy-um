39afa531ad8bee4b282b8e6a078432bc9b40b9de
HeroBuxx <herobuxx@gmail.com>
2020-09-11 16:13:56 +0000

    sepolicy: Change path for AospLime
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- sepolicy.mk
+++ sepolicy.mk
@@ -27,4 +27,4 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
--include device/ayasakura/sepolicy/qcom/sepolicy.mk
+-include device/lime/sepolicy/qcom/sepolicy.mk
